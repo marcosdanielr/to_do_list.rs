5b7784264050be1a885bb931803329f30ed44462
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-08-22 03:37:23 -0300

    feat: create task

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 mod handlers;
 
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-use handlers::show_tasks::show_tasks;
+use handlers::{create_task::create_task, show_tasks::show_tasks};
+use to_do_list::models::NewTask;
 
 #[get("/tasks")]
 async fn show_tasks_route() -> impl Responder {
@@ -11,10 +12,21 @@ async fn show_tasks_route() -> impl Responder {
     HttpResponse::Ok().json(tasks)
 }
 
+#[post("/tasks")]
+async fn create_task_route(task: web::Json<NewTask>) -> impl Responder {
+    create_task(task.title.clone());
+
+    HttpResponse::Ok()
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(show_tasks_route))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(show_tasks_route)
+            .service(create_task_route)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
--- src/models.rs
+++ src/models.rs
@@ -1,13 +1,21 @@
+use std::borrow::Cow;
+
+use crate::schema::tasks;
 use diesel::prelude::*;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
 #[derive(Queryable, Selectable)]
 #[diesel(table_name = crate::schema::tasks)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 #[derive(Debug, Serialize)]
-
 pub struct Task {
     pub id: i32,
     pub title: String,
     pub is_completed: bool,
+}
+
+#[derive(Insertable, Deserialize)]
+#[diesel(table_name = tasks)]
+pub struct NewTask {
+    pub title: String,
 }
